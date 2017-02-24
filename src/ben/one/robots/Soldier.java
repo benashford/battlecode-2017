86bc3b71bab5727cd7bc567818c62c41833005d7
Ben Ashford <benashford@users.noreply.github.com>
2017-02-24 23:31:41 +0000

    Bring back useless soldier

--- src/ben/one/robots/Soldier.java
+++ src/ben/one/robots/Soldier.java
@@ -1,38 +1,30 @@
 package ben.one.robots;
 
-import battlecode.common.Direction;
 import battlecode.common.GameActionException;
 import battlecode.common.RobotController;
-import battlecode.common.RobotInfo;
 import ben.one.Awareness;
 
 public class Soldier extends Robot {
+    private SoldierState state = new Roam();
+
     public Soldier(RobotController rc) {
         super(rc);
     }
 
     void doTurn(Awareness awareness) throws GameActionException {
-        RobotInfo[] enemies = senseEnemies();
-        Direction d = randomDirection();
-        if (enemies.length == 0) {
-            if (rc.canMove(d)) {
-                rc.move(d);
-            }
-        } else {
-            Direction toEnemy = rc.getLocation().directionTo(enemies[0].location);
+        // TODO: defensiveness/state switching
+        state = state.act(awareness);
+    }
 
-            if (rc.canFireSingleShot()) {
-                rc.fireSingleShot(toEnemy);
-            }
-
-            Direction awayFromEnemy = toEnemy.opposite();
-            if (rc.canMove(awayFromEnemy)) {
-                rc.move(awayFromEnemy);
-            } else {
-                if (rc.canMove(d)) {
-                    rc.move(d);
-                }
-            }
+    private class Roam implements SoldierState {
+        @Override
+        public SoldierState act(Awareness awareness) throws GameActionException {
+            defaultMovement(awareness);
+            return this;
         }
     }
+}
+
+interface SoldierState extends RobotState<SoldierState> {
+
 }
