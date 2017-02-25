bcbefcf677fd12073344ed7fb71e577ef5d33ea4
Ben Ashford <benashford@users.noreply.github.com>
2017-02-25 11:39:52 +0000

    Simple indiscriminate firing

--- src/ben/one/robots/Soldier.java
+++ src/ben/one/robots/Soldier.java
@@ -1,9 +1,6 @@
 package ben.one.robots;
 
-import battlecode.common.GameActionException;
-import battlecode.common.RobotController;
-import battlecode.common.RobotInfo;
-import battlecode.common.RobotType;
+import battlecode.common.*;
 import ben.one.Awareness;
 
 import java.util.List;
@@ -36,9 +33,56 @@ public class Soldier extends Robot {
         state = new Roam();
     }
 
-    private void moveAndFire(Awareness awareness) {
+    private float calculateAttractionFactor(RobotInfo enemyBot) {
+        // TODO: implement me
+        return 1.0f;
+    }
+
+    private void moveAroundEnemy(Awareness awareness) throws GameActionException {
+        // RULES: stay away from tanks and lumberjacks
+        // move closer to weaker units
+        MapLocation pos = rc.getLocation();
+        float nextX = pos.x;
+        float nextY = pos.y;
+
         List<RobotInfo> enemy = awareness.findEnemy();
-        debug_outf("Found: %d enemy", enemy.size());
+        for (RobotInfo enemyBot : enemy) {
+            MapLocation enemyPos = enemyBot.getLocation();
+            Direction dir = pos.directionTo(enemyPos);
+            float attractionFactor = calculateAttractionFactor(enemyBot);
+            MapLocation targetPos = pos.add(dir, attractionFactor);
+
+            rc.setIndicatorLine(pos, targetPos, 0, 255, 100);
+
+            nextX += targetPos.x;
+            nextY += targetPos.y;
+        }
+
+        MapLocation target = new MapLocation(nextX, nextY);
+
+        rc.setIndicatorLine(pos, target, 0, 100, 255);
+
+        Direction targetDir = pos.directionTo(target);
+        if (rc.canMove(targetDir)) {
+            rc.move(target);
+        }
+    }
+
+    private void fireAtEnemy(Awareness awareness) throws GameActionException {
+        // TODO: real implementation
+        RobotInfo enemyBot = awareness.findEnemy().get(0);
+        MapLocation enemyPos = enemyBot.getLocation();
+        if (rc.canFireSingleShot()) {
+            rc.fireSingleShot(rc.getLocation().directionTo(enemyPos));
+        }
+    }
+
+    private void moveAndFire(Awareness awareness) throws GameActionException {
+        if (!rc.hasMoved()) {
+            // Unit may have already moved to dodge bullets
+            moveAroundEnemy(awareness);
+        }
+        fireAtEnemy(awareness);
     }
 
     private class Roam implements SoldierState {
